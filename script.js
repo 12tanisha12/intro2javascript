cbff016f026a4d2d1109138e12d983f67955e640
12tanisha12 <163028909+12tanisha12@users.noreply.github.com>
2024-03-12 18:02:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function myFunction(){
+  document.getElementById("greet").style.color="green"; 
+}
+function night(){
+document.body.style.backgroundColor="forestgreen";
+
+}
+function font() {
+document.body.style.fontFamily="helvetica";
+
+
+  
+}
